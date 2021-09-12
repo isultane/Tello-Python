f139c7333c2230a71aaf7c8e60f85af735217bab
Techy <to_sultan@yahoo.com>
2021-09-12 08:16:55 +0300

    installed shell file

--- Tello_Video/install/Linux/linux_install.sh
+++ Tello_Video/install/Linux/linux_install.sh
@@ -8,12 +8,12 @@ cd ..
 sudo apt-get update -y
 
 # install python 2.7
-sudo apt-get install python2.7 python-pip -y
-sudo pip install --upgrade pip
+#sudo apt-get install python2.7 python-pip -y
+#sudo pip install --upgrade pip
 
 #switch to python2.7
-sudo update-alternatives --install /usr/bin/python python /usr/bin/python2 150 
-sudo update-alternatives --install /usr/bin/python python /usr/bin/python3 100
+#sudo update-alternatives --install /usr/bin/python python /usr/bin/python2 150 
+#sudo update-alternatives --install /usr/bin/python python /usr/bin/python3 100
 
 sudo apt-get update -y
 
